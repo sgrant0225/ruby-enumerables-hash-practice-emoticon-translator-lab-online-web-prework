47b5ffa7996556ffe25c885101209963627a12a8
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-31 02:25:02 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,16 +11,26 @@ def load_library(file_path)
 emoticons.each do |meaning, value|
  new_result[:get_meaning][value[1]] = meaning
  new_result[:get_emoticon][value[0]] = value[1]
- #binding.pry
 end
 new_result
 end
 
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  new_result = load_library(file_path)
+  if new_result[:get_emoticon].include? (emoticon)
+  return new_result[:get_emoticon][emoticon] 
+else
+  return "Sorry, that emoticon was not found"
+end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  new_result = load_library(file_path)
+  if new_result[:get_meaning].include? (emoticon)
+    return new_result[:get_meaning][emoticon]
+  else 
+    return "Sorry, that emoticon was not found"
+end
 end
