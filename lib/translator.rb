7438bd848ee4d14f5fd1f44f075a35d823d45aec
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-31 01:46:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   new_result = load_library(file_path)
-  return new_result[:get_meaning[7]]
+  return new_result[:get_meaning
   binding.pry
 end
 
